3d7e6ddee66933437744171ec6ec4e63003eb1de
Tanner <thebow25@gmail.com>
2022-02-09 22:02:53 -0700

    Did the color unity thing plan to add a bot that tells you what buttons

---
+++ SpringClassProject2022/Assets/ColorForPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorForPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.G))
+		{
+			GetComponent<Renderer> ().material.color = Color.green;
+		}
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			GetComponent<Renderer>().material.color = Color.black;
+			
+		}
+
+		if (Input.GetKeyDown(KeyCode.Y))
+		{
+			GetComponent<Renderer>().material.color = Color.yellow;
+		}
+	}
+}
