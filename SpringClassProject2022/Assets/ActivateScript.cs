5cf189667f0f97f784e92c7fc6e45761d9be8117
Tanner <thebow25@gmail.com>
2022-02-10 22:57:04 -0700

    Did a signpost to be able to tell pplayer what color there is

---
+++ SpringClassProject2022/Assets/ActivateScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ActivateScript : MonoBehaviour
+{
+	public UnityEvent line;
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Input.GetKey(KeyCode.E);
+		print("Press G,B,Y for a color change!");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
