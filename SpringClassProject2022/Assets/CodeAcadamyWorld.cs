d4141af3426a8b272bf125a0c5fa89f423e0c0c9
Tanner <thebow25@gmail.com>
2022-02-11 19:27:03 -0700

    Just doing this so I can commit everything I do

---
+++ SpringClassProject2022/Assets/CodeAcadamyWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeAcadamyWorld : MonoBehaviour {
+    private void Start()
+    {
+        Console.WriteLine("Just showing that I did codeacadmy stuff for today and what not");
+    }
+}
