4e962fd74f3cea027a78c3de6c536a8b6ba18dda
Tanner <thebow25@gmail.com>
2022-02-15 20:32:11 -0700

    Did the variable unity thing to get ready for discussion

---
+++ SpringClassProject2022/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+	private int theInt = 20; //Variable that is used for future scripts to achieve said assigned things
+
+	// Use this for initialization
+	void Start ()
+	{
+		theInt = machineFunction(theInt);
+		Debug.Log(theInt);
+
+	}
+	
+	// Update is called once per frame
+	int machineFunction(int number) //Fucntion that takes in process to do an action
+	{
+		int result;
+		result = number * 5;
+		return result;
+	}
+}
