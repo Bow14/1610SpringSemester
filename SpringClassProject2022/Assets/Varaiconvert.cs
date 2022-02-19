7c437737d9365cd4ba9a22f424a0ea9c7df2d292
Tanner <thebow25@gmail.com>
2022-02-18 23:40:36 -0700

    ConvertLine

---
+++ SpringClassProject2022/Assets/Varaiconvert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Varaiconvert : MonoBehaviour {
+	// Ask user for fave number
+	
+	//Console.Write("Enter your favorite number!: ");
+
+	// Turn that answer into an int
+      
+	// Attempt 1: use implicit conversion
+	// int faveNumber = Console.ReadLine();
+      
+	// Attempt 2: use explicit conversion
+	// int faveNumber = (int)Console.ReadLine();
+      
+	// Attempt 3: use Convert method
+	int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
