e34c49cce6f54f8fbc0e18c6b2cf2be7c17b8f82
Tanner <thebow25@gmail.com>
2022-03-01 22:05:52 -0700

    Writing a while loop basic int countup

---
+++ SpringClassProject2022/Assets/Loop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+	private int mathSub = 20;
+	
+
+	// Use this for initialization
+	void Start () {
+		while (mathSub < 39)
+		{
+			print(mathSub);
+			mathSub++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
