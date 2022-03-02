07ff3acdf84c87608f25de15e03d74c8be5205bc
Tanner <thebow25@gmail.com>
2022-03-02 02:29:12 -0700

    Made an array for strings and ints

---
+++ SpringClassProject2022/Assets/Arrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+string[] arrayExam = {"Pencile", "Eraser", "Test"};
+public int[] numbersArray = {12, 13, 145};
+
+private void Start()
+{
+    print(arrayExam);
+}
+}
