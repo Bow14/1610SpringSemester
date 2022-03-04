cbe26dc6d5aed235a27473ebc6b62111a778af9a
Tanner <thebow25@gmail.com>
2022-03-03 22:26:44 -0700

    Experimented with arrays and loops

---
+++ SpringClassProject2022/Assets/ArrayLoops.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayLoops : MonoBehaviour
+{
+	public string[] peopleA = {"John", "Greg", "Pearson", "Caleb"};
+
+	public int[] numberG = {2, 4, 5, 20, 45};
+	private int numberB = 25;
+	
+	
+	// Use this for initialization
+	void Start () {
+		foreach (var p in peopleA)
+		{
+			print(p);
+		}
+
+		while (numberG.Length > numberB )
+		{
+			print("Number G is just better");
+			numberB++;
+
+		}
+
+		for (int i = 0; i < numberB; i++)
+		{
+			print(numberB);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
