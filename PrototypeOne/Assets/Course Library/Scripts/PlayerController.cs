a9295d30bda9c9680411ee8ac785da3eae9a10f7
Tanner <thebow25@gmail.com>
2022-03-15 21:00:44 -0600

    Added and finally fixed the start to this project
    Done 1.1 and 1.2

---
+++ PrototypeOne/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // the code used to move the vehicle forward
+        transform.Translate(35* Time.deltaTime* Vector3.forward);
+        
+    }
+}
