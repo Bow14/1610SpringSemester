ad7b7e49c167b1f570c5b8d05a1c837cb40181cb
Tanner <thebow25@gmail.com>
2022-04-01 19:13:05 -0600

    Fixed and optimized my playermovement script

--- Broken Streets/Assets/Scripts/PlayerController.cs
+++ Broken Streets/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
+    private float speed = 20.0f;
     public float turningSpeed = 50.0f;
-    public float verticalInput; 
+    
+    private Rigidbody playerRB;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        verticalInput = Input.GetAxis("Vertical");
+       playerMovements();
+
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        //transform.Rotate((Vector3.left * turningSpeed * verticalInput * Time.deltaTime));
+    }
+
+    void playerMovements()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");  
         
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        transform.Rotate((Vector3.left * turningSpeed * verticalInput * Time.deltaTime));
+        playerRB.AddForce(Vector3.forward* speed * verticalInput);
+        playerRB.AddForce(Vector3.right * speed * turningSpeed * horizontalInput);
     }
 }
