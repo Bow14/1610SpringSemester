160caf9f0b32d02d433b31048efca851e348f615
Tanner <thebow25@gmail.com>
2022-04-05 21:07:09 -0600

    Finished 4.4

--- Prototype4/Assets/Course Library/Scripts/SpawnManger.cs
+++ Prototype4/Assets/Course Library/Scripts/SpawnManger.cs
@@ -1,19 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManger : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
     
     
     // Start is called before the first frame update
     void Start()
     {
-       
-            
-        Instantiate(enemyPrefab, GenerateSpawnPosition() , enemyPrefab.transform.rotation);
+     SpawnEnemyWave(waveNumber);
+     Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition() , enemyPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
