3cabf7ad98495ca3f6a60a906ff1374467614433
TkBow <thebow25@gmail.com>
2022-04-27 20:18:54 -0600

    Finished prtottype2 with 6.1

--- Prototype2/Assets/Scripts/Destroyoutofbounds.cs
+++ Prototype2/Assets/Scripts/Destroyoutofbounds.cs
@@ -1,30 +1,35 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destroyoutofbounds : MonoBehaviour
+public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float topBound = 35;
+    private float topBound = 30;
+    private float lowerBound = -10;
 
-    private float lowBound = -15;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If an object goes past the players in view games, remove that object 
-        if (transform.position.z > topBound )
+        if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
-        }else if (transform.position.z < lowBound)
+            // Instead of destroying the projectile when it leaves the screen
+            //Destroy(gameObject);
+
+            // Just deactivate it
+            gameObject.SetActive(false);
+
+        }
+        else if (transform.position.z < lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
-        
+
     }
 }
